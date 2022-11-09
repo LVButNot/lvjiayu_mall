d9820efe06f13b85cb659652689624990cdd7272
lvjiayu <364365706@qq.com>
2022-11-09 14:27:09 +0800

    20221109办公室

--- src/main/java/com/lvjiayu/mall/service/impl/OrderServiceImpl.java
+++ src/main/java/com/lvjiayu/mall/service/impl/OrderServiceImpl.java
@@ -1,16 +1,21 @@
 package com.lvjiayu.mall.service.impl;
 
 import com.lvjiayu.mall.common.NewBeeMallException;
+import com.lvjiayu.mall.common.OrderStatusEnum;
+import com.lvjiayu.mall.common.PayTypeEnum;
 import com.lvjiayu.mall.common.ServiceResultEnum;
 import com.lvjiayu.mall.controller.mall.vo.OrderDetailVo;
+import com.lvjiayu.mall.controller.mall.vo.OrderItemVO;
 import com.lvjiayu.mall.dao.OrderItemMapper;
 import com.lvjiayu.mall.dao.OrderMapper;
 import com.lvjiayu.mall.entity.MallOrder;
 import com.lvjiayu.mall.entity.MallOrderItem;
 import com.lvjiayu.mall.service.OrderService;
+import com.lvjiayu.mall.util.BeanUtil;
 import com.lvjiayu.mall.util.PageQueryUtil;
 import com.lvjiayu.mall.util.PageResult;
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
 
 import javax.annotation.Resource;
 import java.util.List;
@@ -38,6 +43,17 @@ public class OrderServiceImpl implements OrderService {
         }
         List<MallOrderItem> orderItems = orderItemMapper.selectByOrderId(mallOrder.getOrderId());
         //获取订单项数据
-
+        if(!CollectionUtils.isEmpty(orderItems)){
+            List<OrderItemVO> orderItemVOS = BeanUtil.copyList(orderItems, OrderItemVO.class);
+            OrderDetailVo orderDetailVo = new OrderDetailVo();
+            BeanUtil.copyProperties(mallOrder, orderDetailVo);
+            orderDetailVo.setOrderStatusString(OrderStatusEnum.getNewBeeMallOrderStatusEnumByStatus(orderDetailVo.getOrderStatus()).getName());
+            orderDetailVo.setPayTypeString(PayTypeEnum.getPayTypeEnumByType(orderDetailVo.getPayType()).getName());
+            orderDetailVo.setOrderItemVOS(orderItemVOS);
+            return orderDetailVo;
+        }else{
+            NewBeeMallException.fail(ServiceResultEnum.ORDER_ITEM_NULL_ERROR.getResult());
+            return null;
+        }
     }
 }
--- src/main/java/com/lvjiayu/mall/util/BeanUtil.java
+++ src/main/java/com/lvjiayu/mall/util/BeanUtil.java
@@ -1,7 +1,9 @@
 package com.lvjiayu.mall.util;
 
-import lombok.Data;
 import org.springframework.beans.BeanUtils;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public abstract class BeanUtil {
@@ -11,5 +13,31 @@ public abstract class BeanUtil {
         }
         BeanUtils.copyProperties(source, target, ignoreProperties);
         return target;
+    }
+    public static <T> List<T> copyList(List source, Class<T> clazz){
+        return copyList(source, clazz, null);
+    }
+    public static <T> List<T> copyList(List sources, Class<T> clazz, Callback<T> callback){
+        List<T> targetList = new ArrayList<>();
+        if(sources != null){
+            try {
+                for(Object source : sources){
+                    T target = clazz.newInstance();
+                    copyProperties(source, target);
+                    if (callback != null) {
+                        callback.set(source, target);
+                    }
+                    targetList.add(target);
+                }
+            } catch (InstantiationException e) {
+                e.printStackTrace();
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            }
+        }
+        return targetList;
+    }
+    public interface Callback<T> {
+        void set(Object source, T target);
     }
 }
